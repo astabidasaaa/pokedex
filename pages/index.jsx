d5d59abbaea026d0d8f784575b2be79d11014eea
astabidasaaa <astabidasabcd@gmail.com>
2023-08-11 21:40:25 +0700

    rm unused script

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,55 +1,21 @@
-"use client";
-
 import React from "react";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { gql } from "@apollo/client";
-import client from "../apollo-client";
-import InfiniteLoading from "@/components/InfiniteLoading";
-import DataFetch from "@/components/DataFetch";
-import { useRouter } from "next/router";
-
-const inter = Inter({ subsets: ["latin"] });
+import PokemonList from "@/components/PokemonList";
+import Head from "next/head";
 
 const Home = ({ query }) => {
-  // console.log(pokemons);
-  const router = useRouter();
-
-  // console.log(query);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Test
-      {/* {pokemons.map((item, index) => {
-        return (
-          <div key={index}>
-            <Image src={item.image} alt={item.name} fill />
-            <p>{item.name}</p>
-            <p>{item.image}</p>
-          </div>
-        );
-      })} */}
-      <DataFetch filter={query} />
-    </main>
+    <>
+      <Head>
+        <title>Pokédex</title>
+      </Head>
+      <PokemonList filter={query} />
+    </>
   );
 };
 
 export default Home;
 
 export const getServerSideProps = async ({ query }) => {
-  // const { data } = await client.query({
-  //   query: gql`
-  //     query Pokemons {
-  //       pokemons(first: 10) {
-  //         id
-  //         name
-  //         image
-  //       }
-  //     }
-  //   `,
-  // });
-
   return {
     props: {
       query: query?.types || null,
